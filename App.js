191561dd74bec27cd1654dbec667369dbfcd1329
Miles Granger <miles59923@gmail.com>
2017-10-28 13:17:53 +0200

    initial commit - base

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
-// @flow
-import boot from "./src/boot/index";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const app = boot();
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
 
-export default app;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
